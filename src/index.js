ec5cc4a50befa01697ab34218e0510a84a567a39
sm-valery <sm-valery@yandex.ru>
2016-11-10 17:36:24 +0300

    урок 2с

--- src/index.js
+++ src/index.js
@@ -9,12 +9,55 @@ app.get('/', (req, res) => {
   });
 });
 
+//урок 2A
 app.get('/t2a',(req,res)=>{
   const sum = (+req.query.a||0) + (+req.query.b||0);
 
   res.send(sum.toString());
 })
 
+//урок 2C
+app.get('/t2c',(req,res)=>{
+
+const urlstr = req.query.username;
+
+  res.send(getUrlUserName(urlstr));
+})
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+
+function getUrlUserName(urlstr){
+
+if(urlstr.indexOf('?') >0 )
+urlstr = urlstr.substring(0,urlstr.indexOf('?'));
+
+
+var uname = urlstr.replace('@','').split('/');
+
+return '@' + uname[uname.length-1];
+
+}
+
+// Пример 1
+
+// ?username=https://vk.com/skillbranch
+// -
+// @skillbranch
+// Пример 2
+
+// ?username=//vk.com/skillbranch
+// -
+// @skillbranch
+// Пример 3
+
+// ?username=skillbranch
+// -
+// @skillbranch
+// Пример 4
+
+// ?username=https://vk.com/skillbranch?w=wall-117903599_1076
+// -
+// @skillbranch
