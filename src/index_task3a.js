ba99843cacc08fc132283f4f5e6335eadf1e2c44
sm-valery <sm-valery@yandex.ru>
2016-11-17 10:14:44 +0300

    Add files via upload

--- src/index_task3a.js
+++ src/index_task3a.js
@@ -2,32 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
-import fetch from 'node-fetch'
+import fetch from 'isomorphic-fetch'
 import _ from 'lodash';
 
 
-// mongoose.Promise = Promise;
-// mongoose.connect('mongodb://publicdb.mgbeta.ru/isuvorov_skb3');
+// const pc = {"board":{"vendor":"IBM","model":"IBM-PC S-100","cpu":{"model":"80286","hz":12000},"image":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/Picture%20of%2080286%20V2%20BoardJPG.jpg","video":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/80286-Demo3.mp4"},"ram":{"vendor":"CTS","volume":1048576,"pins":30},"os":"MS-DOS 1.25","floppy":0,"hdd":[{"vendor":"Samsung","size":33554432,"volume":"C:"},{"vendor":"Maxtor","size":16777216,"volume":"D:"},{"vendor":"Maxtor","size":8388608,"volume":"C:"}],"monitor":null,"length":42,"height":21,"width":54};
 
-// const Pet = mongoose.model('Pet',{
-//   type: String,
-//   name: String
-// });
+let pc = {};
 
-// const kitty = new Pet({
-//   name: 'aaaaa',
-//   type: 'cat'
-// });
-
-// kitty.save()
-// .then(()=>{
-//   console.log('success');
-// })
-// .catch((err)=>{
-//   console.log('err',err);
-// });
-
-const pc = {"board":{"vendor":"IBM","model":"IBM-PC S-100","cpu":{"model":"80286","hz":12000},"image":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/Picture%20of%2080286%20V2%20BoardJPG.jpg","video":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/80286-Demo3.mp4"},"ram":{"vendor":"CTS","volume":1048576,"pins":30},"os":"MS-DOS 1.25","floppy":0,"hdd":[{"vendor":"Samsung","size":33554432,"volume":"C:"},{"vendor":"Maxtor","size":16777216,"volume":"D:"},{"vendor":"Maxtor","size":8388608,"volume":"C:"}],"monitor":null,"length":42,"height":21,"width":54};
+fetch('https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json')
+  .then(async (res) => {
+    pc = await res.json();
+  })
+  .catch(err => {
+    console.log('Чтото пошло не так:', err);
+  });
 
 
 const app = express();
